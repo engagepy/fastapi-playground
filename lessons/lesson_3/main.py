4436f83d296b99d31bd27c3340d80bd577e03b78
Zorawar Purohit <zorawar@m37labs.com>
2026-01-12 15:19:07 +0530

    lesson 3 testing, fix name parameter, lesson 4 initiated

--- lessons/lesson_3/main.py
+++ lessons/lesson_3/main.py
@@ -31,10 +31,10 @@ class ContactParameter(BaseModel):
 
 
 #Route that demonstrates form fields instead of a single body parameter
-@app.post("/greeting/", tags=["Observe request body parameters using Pydantic Model & Enum"])
-async def add_contact(details: ContactParameter, group: ContactGroup = ContactGroup.personal):
+@app.post("/greeting/{name}", tags=["Observe request body parameters using Pydantic Model & Enum"])
+async def add_contact(name: str,details: ContactParameter, group: ContactGroup = ContactGroup.personal):
     contact_info = {
-        "name": details.first_name.casefold(),
+        "name": name.casefold(),
         "lastname": details.last_name.casefold(),
         "number": details.number,
         "email": details.email.casefold(),
