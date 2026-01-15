4ab491d09568bbfecdf38c6f8057b775e0aa25de
Zorawar Purohit <zorawar@m37labs.com>
2026-01-15 21:07:21 +0530

    additonal metadata added to query

--- lessons/lesson_7/router.py
+++ lessons/lesson_7/router.py
@@ -9,9 +9,9 @@ router = APIRouter(
 @router.get(
     "/items/",
     summary="Multiple query params with validation",
-    description="Demonstrates multiple query parameters with uniform validation using FastAPI Query."
+    description="Demonstrates query metadata addition using title, alias, and max_length using Query class."
 )
-async def read_items(q: Annotated[list[str] | None, Query(max_length=20)] = None): # just list use is possible, no str
+async def read_items(q: Annotated[list[str] | None, Query(title= "Observationbs on car", max_length=20, alias="car-damage")] = None): # just list use is possible, no str
     results = {"items": [{"car_number": "HR01X"}, {"service_need": "Oil Change, Tire Rotation"}]}
     if q:
         results.update({"observations": q})
