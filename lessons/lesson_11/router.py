4f8ea2b20cb962c20db3620a8b1b89baf6ab8683
Zorawar Purohit <zorawar@m37labs.com>
2026-01-24 18:09:54 +0530

    furthering lesson 11 query params

--- lessons/lesson_11/router.py
+++ lessons/lesson_11/router.py
@@ -1,11 +1,26 @@
 from typing import Annotated, Literal
 from fastapi import APIRouter, Query 
 
-from pydantic import AfterValidator
-import enum
+from pydantic import BaseModel, Field
 
 router = APIRouter(
     prefix="/lesson-11",
     tags=["Lesson 11"]
 )
+
+
+class MovieRequest(BaseModel):
+    title: str = Field(..., title="Movie Title", max_length=100)
+    director: str = Field(..., title="Director Name", max_length=50)
+    year: int = Field(..., title="Release Year", ge=1900, le=2100)
+
+@router.get(
+    "/movie/",
+    summary="Path parameter validation using Literal and AfterValidator",
+    description="Demonstrates path parameter validation using Literal from typing and BaseModel, Field from Pydantic."
+)
+async def find_movie(
+    q: Annotated[MovieRequest, Query(title="Movie details in the request body")]
+):
+    return {"movie" : q.model_dump()}
 
