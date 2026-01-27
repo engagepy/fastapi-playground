746bb70f8197f83f337897e404b1630373a25986
Zorawar Purohit <zorawar@m37labs.com>
2026-01-28 00:01:05 +0530

    added model_config, Literal

--- lessons/lesson_11/router.py
+++ lessons/lesson_11/router.py
@@ -10,15 +10,18 @@ router = APIRouter(
 
 
 class MovieRequest(BaseModel):
+    model_config = {"extra": "forbid"}  # to forbid extra fields in the request body
     title: str = Field(..., title="Movie Title", max_length=100)
     director: str = Field(..., title="Director Name", max_length=50)
     year: int = Field(..., title="Release Year", ge=1900, le=2100)
 
 @router.get(
-    "/movie/",
+    "/get_movie/",
     summary="Path parameter validation using Literal and AfterValidator",
     description="Demonstrates path parameter validation using Literal from typing and BaseModel, Field from Pydantic."
 )
+
+# for beginners `q` is query parameter here and acting as request body via MovieRequest pydantic model defined above
 async def find_movie(
     q: Annotated[MovieRequest, Query(title="Movie details in the request body")]
 ):
