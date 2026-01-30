5da97a6eb60730880c551aa4e7f66256aff99b95
Zorawar Purohit <zorawar@m37labs.com>
2026-01-30 22:34:43 +0530

    pydantic path, body and query parameter example

--- lessons/lesson_12/router.py
+++ lessons/lesson_12/router.py
@@ -1,5 +1,5 @@
 from typing import Annotated, Literal
-from fastapi import APIRouter, Query
+from fastapi import APIRouter, Query, Path, Body
 from pydantic import BaseModel, Field
 import enum
 import random
@@ -9,3 +9,24 @@ router = APIRouter(
     tags=["Lesson 12"]
 )
 
+class Basket(BaseModel):
+    id: int
+    items: list[str] = Field(..., description="List of items in the basket")
+    total_price: float = Field(..., description="Total price of the basket")
+
+
+@router.put("/baskets/{basket_id}", response_model=Basket)
+async def get_basket(
+    basket_id: Annotated[int, Path(gt=0,le=100, description="The ID of the basket to retrieve")],
+    q : str | None = Query(default=None, max_length=50, description="Optional query string to filter results"),
+    basket: Basket | None = None
+):
+    result = {"id" : basket_id}
+    if q:
+        result.update({"query": q})
+    if basket:
+        result.update({"basket": basket})
+    return result
+
+
+    
