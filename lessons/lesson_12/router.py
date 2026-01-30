ebd267bac47c6054fd164f7127841f7999ac1098
Zorawar Purohit <zorawar@m37labs.com>
2026-01-30 22:49:08 +0530

    mix path, query, body, validation with Path and Query + Annotated from typing

--- lessons/lesson_12/router.py
+++ lessons/lesson_12/router.py
@@ -9,13 +9,28 @@ router = APIRouter(
     tags=["Lesson 12"]
 )
 
+
+section = enum.Enum(
+    "section",
+    {
+        "fruits": 1,
+        "vegetables": 2,
+        "dairy": 3,
+        "bakery": 4
+    }
+)
+
 class Basket(BaseModel):
-    id: int
+    item_zone: section = Field(..., description="Section of the basket")
     items: list[str] = Field(..., description="List of items in the basket")
     total_price: float = Field(..., description="Total price of the basket")
 
 
-@router.put("/baskets/{basket_id}", response_model=Basket)
+@router.put(
+        "/baskets/{basket_id}",
+        summary="Mix Path, Query, and Body parameter with 'Path','Query validation",
+        description="Demonstrates path, query, and body parameter validation using Pydantic models"
+        )
 async def get_basket(
     basket_id: Annotated[int, Path(gt=0,le=100, description="The ID of the basket to retrieve")],
     q : str | None = Query(default=None, max_length=50, description="Optional query string to filter results"),
