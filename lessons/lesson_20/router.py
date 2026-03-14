38ede4b77f41bd348b51a73e232620a6fae2cc98
Zorawar Purohit <zorawar@m37labs.com>
2026-03-14 22:50:25 +0530

    bodies of pure lists lesson 20

--- lessons/lesson_20/router.py
+++ lessons/lesson_20/router.py
@@ -1,8 +1,20 @@
 from fastapi import APIRouter
-from pydantic import BaseModel
+from pydantic import BaseModel, HttpUrl
 
 router = APIRouter(
     prefix="/lesson-20",
     tags=["Lesson 20"]  
 )
+
+class ImageRequest(BaseModel):
+    url: HttpUrl
+    name: str
+
+@router.post(
+    "images/upload-multiple",
+    summary="Upload Multiple Images",
+    description="Endpoint to upload multiple images at once."
+    )
+async def upload_multiple_images(images: list[ImageRequest]):
+    return {"message": f"Received {len(images)} images for upload."}
 
