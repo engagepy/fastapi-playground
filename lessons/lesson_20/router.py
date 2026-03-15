86bb2cbde2aefe265b287eac3775754063b2d6a8
Zorawar Purohit <zorawar@m37labs.com>
2026-03-15 15:37:19 +0530

    arbitrary dict

--- lessons/lesson_20/router.py
+++ lessons/lesson_20/router.py
@@ -12,9 +12,20 @@ class ImageRequest(BaseModel):
 
 @router.post(
     "images/upload-multiple",
-    summary="Upload Multiple Images",
+    summary="Upload Multiple Images Pur Lists []",
     description="Endpoint to upload multiple images at once."
     )
 async def upload_multiple_images(images: list[ImageRequest]):
     return {"message": f"Received {len(images)} images for upload."}
+
+@router.put(
+    "/index-weights",
+    summary="Update Index Weights Dict with int keys and float values",
+    description="Endpoint to update index weights where the keys are integers and the values are floats."
+)
+async def update_index_weights(weights: dict[int, float]):
+    return weights
+
+
+
 
