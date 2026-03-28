439e6857d1cd5e0c5ce5ea3ef1a0695aca64ff58
engagepy <42845567+engagepy@users.noreply.github.com>
2026-03-28 22:28:18 +0530

    Body with multiple examples

---
+++ lessons/lesson_22/router.py
@@ -0,0 +1,37 @@
+from typing import Annotated
+from fastapi import APIRouter, Body , Path, Query
+from pydantic import BaseModel
+
+router = APIRouter(
+    prefix="/lesson-22",
+    tags=["Lesson 22"]
+)
+
+class Item(BaseModel):
+    name : str
+    description : str | None = None
+    price : float 
+    tax : float | None = None
+
+@router.post(
+    "/items/{item_id}",
+    summary="Create an Item",
+    description="Observe Body Examples in the OpenAPI docs"
+)
+async def udpate_item(
+    item_id: Annotated[int, Path(gt=0, le=100, description="The ID of the item to update")],
+    item: Annotated[
+        Item,
+        Body(
+            example=
+                {
+                    "name": "Item 1",
+                    "description": "This is item 1",
+                    "price": 10.5,
+                    "tax": 0.5
+                })
+            ]
+):
+    results = {"item_id": item_id, "item":item}
+    return results
+    
