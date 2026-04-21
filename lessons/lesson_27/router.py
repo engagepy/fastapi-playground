82e452f547b45fda073c159001ecd228d8efcff8
engagepy <42845567+engagepy@users.noreply.github.com>
2026-04-21 18:25:06 +0530

    Header Pydantic Model Complete

--- lessons/lesson_27/router.py
+++ lessons/lesson_27/router.py
@@ -9,3 +9,24 @@ router = APIRouter(
 )   
 
 
+class CommonHeaders(BaseModel):
+    model_config = {"extra": "forbid"}
+
+    host: str
+    save_data: str | None = None
+    if_modified_since: str | None = None
+    traceparent: str | None = None
+    x_tag: list[str] = None
+
+
+@router.get(
+    "/items",
+    summary="Header Pydantic Model",
+    description="Observe how to use Header parameters with a Pydantic model and forbid extra headers"
+)
+async def read_items(headers: Annotated[CommonHeaders, Header()]):
+    return headers
+
+
+
+
